51204a3626efee8734565d855b849aa20ae90d9a
WOLVERINE0000 <128771086+WOLVERINE0000@users.noreply.github.com>
2023-04-26 23:46:29 +0530

    Add files via upload

---
+++ AC1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,l;
+   printf("Enter two numbers\n"); 
+   scanf("%d%d”,&a,&b);
+if(a>b) 
+{ printf(" A is larger")
+l=a 
+} 
+else
+{printf(" B is larger") 
+l=b
+} 
+   printf(" The large number is %d ”, l); 
+return 0;
+}
